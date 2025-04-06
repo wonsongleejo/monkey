4ab3c0dfb80f61862f048a6b1df666650a3c005e
jiyun <jiyyy0720@naver.com>
2025-04-07 03:04:45 +0900

    store-reservation-service 모듈 설정

---
+++ store-reservation-service/src/test/java/com/monkey/storereservationservice/StoreReservationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.monkey.storereservationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreReservationServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
