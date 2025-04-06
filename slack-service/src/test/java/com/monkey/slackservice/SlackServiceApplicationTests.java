cd75131e86dd4b38441b3f0bad38803674b62f79
jiyun <jiyyy0720@naver.com>
2025-04-07 03:22:07 +0900

    slack-service 모듈 설정

---
+++ slack-service/src/test/java/com/monkey/slackservice/SlackServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.monkey.slackservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlackServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
