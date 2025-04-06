bf6d43d3b92dfce70d66e558156c85c921c1f365
jiyun <jiyyy0720@naver.com>
2025-04-07 03:19:16 +0900

    product-reservation-service 모듈 설정

---
+++ product-reservation-service/src/main/java/com/monkey/productreservationservice/ProductReservationServiceApplication.java
@@ -0,0 +1,15 @@
+package com.monkey.productreservationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication(scanBasePackages = "com.monkey")
+@EnableFeignClients
+public class ProductReservationServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProductReservationServiceApplication.class, args);
+    }
+
+}
