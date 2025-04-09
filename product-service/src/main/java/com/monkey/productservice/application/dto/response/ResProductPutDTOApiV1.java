d6fbd2a0cd5ec07f1a2827c1894ab1099e0ca44f
jiyun <jiyyy0720@naver.com>
2025-04-09 15:20:16 +0900

    feat: 상품 수정 요청/응답 DTO 생성

---
+++ product-service/src/main/java/com/monkey/productservice/application/dto/response/ResProductPutDTOApiV1.java
@@ -0,0 +1,39 @@
+package com.monkey.productservice.application.dto.response;
+
+import com.monkey.productservice.domain.entity.ProductEntity;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ResProductPutDTOApiV1 {
+    private Product product;
+
+    public static ResProductPutDTOApiV1 of(ProductEntity productEntity) {
+        return ResProductPutDTOApiV1.builder()
+                .product(Product.from(productEntity))
+                .build();
+    }
+
+    @Getter
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class Product {
+        private String productName;
+        private Integer price;
+        private Integer quantity;
+
+        public static Product from(ProductEntity entity) {
+            return Product.builder()
+                    .productName(entity.getProductName())
+                    .price(entity.getPrice())
+                    .quantity(entity.getQuantity())
+                    .build();
+        }
+    }
+}
