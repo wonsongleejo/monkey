5ff3f0e44b7a56aab3e089e4c043414fdf0f1d12
jiyun <jiyyy0720@naver.com>
2025-04-09 21:09:33 +0900

    feat: 상품 전체조회 응답 DTO

---
+++ product-service/src/main/java/com/monkey/productservice/application/dto/response/ResProductGetDTOApiV1.java
@@ -0,0 +1,52 @@
+package com.monkey.productservice.application.dto.response;
+
+
+import com.monkey.productservice.domain.entity.ProductEntity;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ResProductGetDTOApiV1 {
+    private List<Product> productList;
+
+    public static ResProductGetDTOApiV1 of(List<ProductEntity> productEntityList) {
+        return ResProductGetDTOApiV1.builder()
+                .productList(Product.from(productEntityList))
+                .build();
+    }
+
+    @Getter
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class Product {
+        private UUID productId;
+        private UUID storeId;
+        private String productName;
+        private Integer price;
+        private Integer quantity;
+
+        public static Product from(ProductEntity productEntity) {
+            return Product.builder()
+                    .productId(productEntity.getProductId())
+                    .storeId(productEntity.getStoreId())
+                    .productName(productEntity.getProductName())
+                    .price(productEntity.getPrice())
+                    .quantity(productEntity.getQuantity())
+                    .build();
+        }
+
+        public static List<Product> from(List<ProductEntity> productEntityList) {
+            return productEntityList.stream().map(Product::from).collect(Collectors.toList());
+        }
+    }
+}
