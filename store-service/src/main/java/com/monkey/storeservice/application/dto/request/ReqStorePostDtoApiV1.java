7f4491c2f7926a0bb689b74cfd78cf7cb0683e3a
Taehoon <lth2943@naver.com>
2025-04-09 15:23:38 +0900

    feat: 팝업스토어 등록 DTO

---
+++ store-service/src/main/java/com/monkey/storeservice/application/dto/request/ReqStorePostDtoApiV1.java
@@ -0,0 +1,51 @@
+package com.monkey.storeservice.application.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ReqStorePostDtoApiV1 {
+
+  @NotBlank
+  private String storeName;
+
+  @NotBlank
+  private String description;
+
+  @NotNull
+  private OpenStatus openStatus;
+
+  @NotNull
+  private LocalDate startDate;
+
+  @NotNull
+  private LocalDate endDate;
+
+  @NotNull
+  private LocalTime startTime;
+
+  @NotNull
+  private LocalTime endTime;
+
+  @NotNull
+  private Integer totalPerson;
+
+  public enum OpenStatus {
+    OPEN, CLOSE;
+
+    public static OpenStatus from(String value) {
+      return OpenStatus.valueOf(value.toUpperCase());
+    }
+  }
+}
