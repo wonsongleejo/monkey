654900abd309bdf83dc99c4317a1d481c77b6239
jiyun <jiyyy0720@naver.com>
2025-04-09 15:20:16 +0900

    feat: 상품 수정 요청/응답 DTO 생성

---
+++ product-service/src/main/java/com/monkey/productservice/application/dto/request/ReqProductPutDTOApiV1.java
@@ -0,0 +1,44 @@
+package com.monkey.productservice.application.dto.request;
+
+import com.monkey.productservice.domain.entity.ProductEntity;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class ReqProductPutDTOApiV1 {
+
+    @Valid
+    @NotNull(message = "상품 정보를 입력해주세요.")
+    private ReqProductPutDTOApiV1.Product product;
+
+    @Getter
+    @Builder
+    @AllArgsConstructor
+    public static class Product {
+        @NotBlank(message = "상품명을 입력해주세요.")
+        @Size(max = 30, message = "상품명은 최대 30자까지 입력 가능합니다.")
+        private String productName;
+
+        @NotNull(message = "상품 가격을 입력해주세요.")
+        @Min(value = 0, message = "상품 가격은 0원 이상이어야 합니다.")
+        private Integer price;
+
+        @NotNull(message = "상품 수량을 입력해주세요.")
+        @Min(value = 0, message = "상품 수량은 0 이상이어야 합니다.")
+        private Integer quantity;
+
+        public void update(ProductEntity productEntity) {
+            productEntity.setProductName(productName);
+            productEntity.setPrice(price);
+            productEntity.setQuantity(quantity);
+        }
+    }
+}
