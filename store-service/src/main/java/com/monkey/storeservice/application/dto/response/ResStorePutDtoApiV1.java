f0eed87670d17848fb2546e14a0915655040ec6b
Taehoon <lth2943@naver.com>
2025-04-11 06:36:12 +0900

    feat: 팝업스토어 수정 응답,요청 Dto

---
+++ store-service/src/main/java/com/monkey/storeservice/application/dto/response/ResStorePutDtoApiV1.java
@@ -0,0 +1,38 @@
+package com.monkey.storeservice.application.dto.response;
+
+import com.monkey.storeservice.domain.article.entity.StoreEntity;
+import lombok.*;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.UUID;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ResStorePutDtoApiV1 {
+
+  private UUID storeId;
+  private String storeName;
+  private String description;
+  private StoreEntity.OpenStatus openStatus;
+  private LocalDate startDate;
+  private LocalDate endDate;
+  private LocalTime startTime;
+  private LocalTime endTime;
+  private Integer totalPersonCount;
+
+  public static ResStorePutDtoApiV1 of(StoreEntity storeEntity) {
+    return ResStorePutDtoApiV1.builder()
+        .storeName(storeEntity.getStoreName())
+        .description(storeEntity.getDescription())
+        .openStatus(storeEntity.getOpenStatus())
+        .startDate(storeEntity.getStartDate())
+        .endDate(storeEntity.getEndDate())
+        .startTime(storeEntity.getStartTime())
+        .endTime(storeEntity.getEndTime())
+        .totalPersonCount(storeEntity.getTotalPersonCount())
+        .build();
+  }
+}
