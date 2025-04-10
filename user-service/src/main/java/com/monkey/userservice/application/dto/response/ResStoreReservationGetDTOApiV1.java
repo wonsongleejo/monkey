423535c5dd7451facad7a92ccd153d129316c8f6
cho <websera12@gmail.com>
2025-04-10 15:11:03 +0900

    feat: 회원 본인의 팝업 스토어 예약 내역 조회 DTO, Controller 구현

---
+++ user-service/src/main/java/com/monkey/userservice/application/dto/response/ResStoreReservationGetDTOApiV1.java
@@ -0,0 +1,40 @@
+package com.monkey.userservice.application.dto.response;
+
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.List;
+import java.util.UUID;
+
+@Getter
+@Builder
+public class ResStoreReservationGetDTOApiV1 {
+
+    private List<StoreReservation> storeReservationList;
+
+    @Getter
+    @Builder
+    public static class StoreReservation {
+        private Long userId;
+        private UUID storeReservationId;
+        private String visitStatus;
+        private TimeSlot timeSlot;
+
+        @Getter
+        @Builder
+        public static class TimeSlot {
+            private Store store;
+            private LocalDate date;
+            private LocalTime entryTime;
+            private LocalTime exitTime;
+
+            @Getter
+            @Builder
+            public static class Store {
+                private UUID storeId;
+            }
+        }
+    }
+}
