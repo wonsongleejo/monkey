2f2328fe46cc7025b6ca1ff98e610f991f404f63
jiyun <jiyyy0720@naver.com>
2025-04-11 02:06:43 +0900

    feat: 상품 예약 요청 DTO

---
+++ product-reservation-service/src/main/java/com/monkey/productreservationservice/application/dto/request/ReqProductReservationPostDTOApiV1.java
@@ -0,0 +1,27 @@
+package com.monkey.productreservationservice.application.dto.request;
+
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ReqProductReservationPostDTOApiV1 {
+    @Valid
+    @NotNull(message = "예약 정보를 입력해주세요.")
+    private ProductReservation productReservation;
+
+    @Getter
+    @Setter
+    public static class ProductReservation {
+        @NotNull(message = "구매할 수량을 입력하세요.")
+        @Min(value = 1, message = "구매 수량은 1개 이상이어야 합니다.")
+        private Integer quantity; // 최대 수량 제한은 서비스에서 처리
+    }
+}
