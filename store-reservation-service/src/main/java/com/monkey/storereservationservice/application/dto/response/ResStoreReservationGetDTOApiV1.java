1a4a011910ad5471413fb54056f95685c21592d3
yejiscore <yejisong24@gmail.com>
2025-04-10 15:03:03 +0900

    refactor: 예약 목록 조회 정적 팩토리 패턴으로 변경

--- store-reservation-service/src/main/java/com/monkey/storereservationservice/application/dto/response/ResStoreReservationGetDTOApiV1.java
+++ store-reservation-service/src/main/java/com/monkey/storereservationservice/application/dto/response/ResStoreReservationGetDTOApiV1.java
@@ -4,29 +4,88 @@ import com.monkey.storereservationservice.domain.StoreReservation.entity.StoreRe
 import lombok.Builder;
 import lombok.Getter;
 
+import java.util.List;
 import java.util.UUID;
 
 @Getter
 @Builder
 public class ResStoreReservationGetDTOApiV1 {
 
-    private Long userId;
-    private UUID storeReservationId;
-    private StoreReservationStatus status;
-    private TimeSlot timeSlot;
+    private List<StoreReservation> storeReservationList;
 
-    @Getter
-    @Builder
-    public static class TimeSlot {
-        private Store store;
-        private String date;
-        private String startTime;
-        private String endTime;
+    public static ResStoreReservationGetDTOApiV1 of() {
+        List<UUID> tmpList = List.of(UUID.randomUUID(), UUID.randomUUID());
+
+        return ResStoreReservationGetDTOApiV1.builder()
+                .storeReservationList(StoreReservation.from(tmpList))
+                .build();
     }
 
     @Getter
     @Builder
-    public static class Store {
-        private UUID storeId;
+    public static class StoreReservation {
+        private UUID storeReservationId;
+        private StoreReservationStatus status;
+        private TimeSlot timeSlot;
+        private User user;
+
+        public static StoreReservation from() {
+            return StoreReservation.builder()
+                    .storeReservationId(UUID.randomUUID())
+                    .status(StoreReservationStatus.SCHEDULED)
+                    .timeSlot(TimeSlot.from())
+                    .user(User.from())
+                    .build();
+        }
+
+        public static List<StoreReservation> from(List<UUID> tmpList) {
+//            List<UUID> tmpList = List.of(UUID.randomUUID(), UUID.randomUUID());
+
+            return tmpList.stream()
+                    .map(id -> StoreReservation.from())
+                    .toList();
+        }
+
+        @Getter
+        @Builder
+        public static class TimeSlot {
+            private Store store;
+            private String date;
+            private String startTime;
+            private String endTime;
+
+            public static TimeSlot from() {
+                return TimeSlot.builder()
+                        .store(Store.from())
+                        .date("0")
+                        .startTime("0")
+                        .endTime("0")
+                        .build();
+            }
+
+            @Getter
+            @Builder
+            public static class Store {
+                private UUID storeId;
+
+                public static Store from() {
+                    return Store.builder()
+                            .storeId(UUID.randomUUID())
+                            .build();
+                }
+            }
+        }
+
+        @Getter
+        @Builder
+        public static class User {
+            private Long userId;
+
+            public static User from() {
+                return User.builder()
+                        .userId(1L)
+                        .build();
+            }
+        }
     }
 }
