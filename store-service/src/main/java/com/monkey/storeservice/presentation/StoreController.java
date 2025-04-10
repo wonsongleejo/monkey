9e273ef96955548e45d9c0094a71dcdb03b8468d
Taehoon <lth2943@naver.com>
2025-04-11 06:37:06 +0900

    feat: 팝업스토어 Controller 수정

--- store-service/src/main/java/com/monkey/storeservice/presentation/StoreController.java
+++ store-service/src/main/java/com/monkey/storeservice/presentation/StoreController.java
@@ -2,6 +2,7 @@ package com.monkey.storeservice.presentation;
 
 import com.monkey.commonmodule.dto.ResDTO;
 import com.monkey.storeservice.application.dto.request.ReqStorePostDtoApiV1;
+import com.monkey.storeservice.application.dto.request.ReqStorePutDtoApiV1;
 import com.monkey.storeservice.application.dto.response.ResStoreGetDtoApiV1;
 import com.monkey.storeservice.application.dto.response.ResStorePutDtoApiV1;
 import com.monkey.storeservice.application.service.StoreServiceApi;
@@ -38,38 +39,47 @@ public class StoreController {
         .startTime(LocalTime.parse("10:00"))
         .endTime(LocalTime.parse("18:00"))
         .openStatus(OpenStatus.CLOSED)
+        .totalPersonCount(50)
         .build();
-    ResStorePostDtoApiV1 response = ResStorePostDtoApiV1.of(storeEntity.getStoreId());
+
+//    // DB사용시에 저장하기 위한 메서드
+//    StoreEntity storeEntity = reqStorePostDtoApiV1.getStore().toEntity();
+
+    ResStorePostDtoApiV1 resDto = ResStorePostDtoApiV1.of(storeEntity);
 
     return new ResponseEntity<>(
-        ResDTO.success(response),
+        ResDTO.success(resDto),
         HttpStatus.OK
     );
 
   }
 
-//  // 팝업스토어 수정
-//  @PutMapping("/{storeId}")
-//  public ResponseEntity<ResDTO<ResStorePutDtoApiV1>> putById(
-//      @PathVariable UUID storeId,
-//      @RequestBody ReqStorePostDtoApiV1 reqStorePostDtoApiV1) {
-//    StoreEntity storeEntity = StoreEntity.builder()
-//        .storeName("오징어게임 팝업")
-//        .description("오징어게임 캐릭터가 있어요")
-//        .startDate(LocalDate.parse("2025-04-10"))
-//        .endDate(LocalDate.parse("2025-04-17"))
-//        .startTime(LocalTime.parse("10:00"))
-//        .endTime(LocalTime.parse("`18:00"))
-//        .openStatus(OpenStatus.OPEN)
-//        .build();
-//
-//    ResStorePutDtoApiV1 response = ResStorePutDtoApiV1.of(storeEntity);
-//
-//    return new ResponseEntity<>(
-//        ResDTO.success(response),
-//        HttpStatus.OK
-//    );
-//  }
+  // 팝업스토어 수정
+  @PutMapping("/{storeId}")
+  public ResponseEntity<ResDTO<ResStorePutDtoApiV1>> putById(
+      @PathVariable UUID storeId,
+      @RequestBody ReqStorePutDtoApiV1 reqStorePutDtoApiV1) {
+    StoreEntity storeEntity = StoreEntity.builder()
+        .storeName("오징어게임 팝업")
+        .description("오징어게임 캐릭터가 있어요")
+        .startDate(LocalDate.parse("2025-04-10"))
+        .endDate(LocalDate.parse("2025-04-17"))
+        .startTime(LocalTime.parse("10:00"))
+        .endTime(LocalTime.parse("18:00"))
+        .openStatus(OpenStatus.OPEN)
+        .totalPersonCount(100)
+        .build();
+
+    //Put요청 Dto에 있는 update메서드로 db업데이트
+    reqStorePutDtoApiV1.getStore().update(storeEntity);
+
+    ResStorePutDtoApiV1 resDto = ResStorePutDtoApiV1.of(storeEntity);
+
+    return new ResponseEntity<>(
+        ResDTO.success(resDto),
+        HttpStatus.OK
+    );
+  }
 
   // 팝업스토어 조회
   @GetMapping("/{storeId}")
@@ -84,21 +94,22 @@ public class StoreController {
         .endDate(LocalDate.parse("2025-05-09"))
         .startTime(LocalTime.parse("10:00"))
         .endTime(LocalTime.parse("18:00"))
-        .totalPerson(100)
+        .totalPersonCount(100)
         .openStatus(StoreEntity.OpenStatus.OPEN)
         .build();
 
     // DTO 변환
-    ResStoreGetDtoApiV1 response = ResStoreGetDtoApiV1.of(storeEntity);
+    ResStoreGetDtoApiV1 resDto = ResStoreGetDtoApiV1.of(storeEntity);
 
     return new ResponseEntity<>(
-        ResDTO.success(response),
+        ResDTO.success(resDto),
         HttpStatus.OK
     );
   }
   // 팝업스토어 전체 조회
   @GetMapping
-  public ResponseEntity<ResDTO<List<ResStoreGetDtoApiV1>>>getBy() {
+  public ResponseEntity<ResDTO<ResStoreGetDtoApiV1>>getBy() {
+
     List<StoreEntity> storeList = new ArrayList<>();
 
     for (int i = 0; i < 5; i++) {
@@ -106,19 +117,19 @@ public class StoreController {
           .storeId(UUID.randomUUID())
           .storeName("루피 팝업스토어 " + i)
           .description("귀여운 굿즈가 가득한 팝업 " + i)
-          .startDate(LocalDate.of(2025, 4, 9))
-          .endDate(LocalDate.of(2025, 4, 16))
-          .startTime(LocalTime.of(10, 0))
-          .endTime(LocalTime.of(18, 0))
-          .totalPerson(100 + i * 10)
+          .startDate(LocalDate.parse("2025-04-09"))
+          .endDate(LocalDate.parse("2025-05-09"))
+          .startTime(LocalTime.parse("10:00"))
+          .endTime(LocalTime.parse("18:00"))
+          .totalPersonCount(10 + i * 10)
           .openStatus(StoreEntity.OpenStatus.OPEN)
           .build()
       );
     }
-    List<ResStoreGetDtoApiV1> response = ResStoreGetDtoApiV1.of(storeList);
+    ResStoreGetDtoApiV1 resDto = ResStoreGetDtoApiV1.of(storeList);
 
     return new ResponseEntity<>(
-        ResDTO.success(response),
+        ResDTO.success(resDto),
         HttpStatus.OK
     );
   }
