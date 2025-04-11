d8c7993cc80de541345cc12581b4c2e7e8a50a1a
cho <websera12@gmail.com>
2025-04-12 01:27:12 +0900

    test: 회원 테스트 코드 작성
    - AuthController 테스트 코드
    - UserController 테스트 코드

---
+++ user-service/src/test/java/com/monkey/userservice/domain/user/controller/UserControllerApiV1Test.java
@@ -0,0 +1,240 @@
+package com.monkey.userservice.domain.user.controller;
+
+import com.epages.restdocs.apispec.ResourceSnippetParameters;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.monkey.userservice.application.dto.request.ReqUserPutDTOApiV1;
+import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders;
+import org.springframework.restdocs.payload.JsonFieldType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static com.epages.restdocs.apispec.ResourceDocumentation.resource;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+
+@SpringBootTest
+@AutoConfigureRestDocs
+@AutoConfigureMockMvc
+@Transactional
+@ActiveProfiles("test")
+public class UserControllerApiV1Test {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    // 회원 전제조회
+    @Test
+    public void testUserGetSuccess() throws Exception {
+
+        mockMvc.perform(
+                        RestDocumentationRequestBuilders.get("/v1/users")
+                )
+                .andExpectAll(
+                        MockMvcResultMatchers.status().isOk(),
+                        MockMvcResultMatchers.jsonPath("code").value("000")
+                )
+                .andDo(
+                        document(
+                                "USER 회원 전체조회 성공",
+                                preprocessRequest(prettyPrint()),
+                                preprocessResponse(prettyPrint()),
+                                resource(
+                                        ResourceSnippetParameters.builder()
+                                                .tag("USER v1")
+                                                .summary("USER 리스트 조회")
+                                                .description("""
+                                                        ## USER 회원 전체조회 엔드포인트 입니다.
+                                                        
+                                                        ---
+                                                        
+                                                        추후 Page 적용 예정
+                                                        
+                                                        """)
+                                                .build()
+                                )
+
+                        )
+                );
+    }
+
+    // 회원 상세조회
+    @Test
+    public void testUserGetByIdSuccess() throws Exception {
+        mockMvc.perform(
+                RestDocumentationRequestBuilders.get("/v1/users/1")
+
+        )
+        .andExpectAll(
+                MockMvcResultMatchers.status().isOk(),
+                MockMvcResultMatchers.jsonPath("code").value("000")
+        )
+        .andDo(
+                document(
+                        "USER 회원 상세조회 성공",
+                        preprocessRequest(prettyPrint()),
+                        preprocessResponse(prettyPrint()),
+                        resource(
+                                ResourceSnippetParameters.builder()
+                                        .tag("USER v1")
+                                        .summary("""
+                                                ## USER 회원 상세조회 엔드포인트 입니다.
+                                                """)
+                                        .build()
+                        )
+
+                )
+        );
+    }
+
+    // 회원 정보 수정
+    @Test
+    public void testUserPutByIdSuccess() throws Exception {
+        ReqUserPutDTOApiV1 reqDto = ReqUserPutDTOApiV1.builder()
+                .user(
+                        ReqUserPutDTOApiV1.User.builder()
+                                .password("testMod123")
+                                .slackId("slackIdMod")
+                                .build()
+                )
+                .build();
+
+        String reqDtoJson = objectMapper.writeValueAsString(reqDto);
+
+        mockMvc.perform(
+                RestDocumentationRequestBuilders.put("/v1/users/1")
+                        .content(reqDtoJson)
+                        .contentType(MediaType.APPLICATION_JSON)
+                )
+                .andExpectAll(
+                        MockMvcResultMatchers.status().isOk(),
+                        MockMvcResultMatchers.jsonPath("code").value("000")
+                )
+                .andDo(
+                        document("USER 회원 정보 수정",
+                                preprocessRequest(prettyPrint()),
+                                preprocessResponse(prettyPrint()),
+                                resource(ResourceSnippetParameters.builder()
+                                        .tag("USER v1")
+                                        .summary("USER 회원 정보 수정")
+                                        .description("""
+                                                ## USER 회원 정보 수정 엔드포인트 입니다.
+                                                
+                                                ---
+                                                
+                                                password, slackId를 입력받아 회원정보를 수정합니다.
+                                                """)
+                                        .requestFields(
+                                                fieldWithPath("user.password").type(JsonFieldType.STRING).description("사용자 비밀번호"),
+                                                fieldWithPath("user.slackId").type(JsonFieldType.STRING).description("사용자 슬랙 아이디")
+                                        )
+                                        .build()
+                                )
+                        )
+                 );
+    }
+
+    // 회원 탈퇴
+    @Test
+    public void testUserDeleteByIdSuccess() throws Exception {
+        mockMvc.perform(
+                RestDocumentationRequestBuilders.delete("/v1/users/1")
+        )
+        .andExpectAll(
+                MockMvcResultMatchers.status().isOk(),
+                MockMvcResultMatchers.jsonPath("code").value("000")
+        )
+        .andDo(
+                document(
+                        "USER 회원 탈퇴 성공",
+                        preprocessRequest(prettyPrint()),
+                        preprocessResponse(prettyPrint()),
+                        resource(
+                                ResourceSnippetParameters.builder()
+                                        .tag("USER v1")
+                                        .summary("""
+                                        ## USER 회원 탈퇴 엔드포인트 입니다.
+                                        """)
+                                        .build()
+                        )
+
+                )
+        );
+    }
+
+    // 팝업 스토어 예약 내역 조회
+    @Test
+    public void tesStoreReservationGetByIdSuccess() throws Exception {
+        mockMvc.perform(
+                        RestDocumentationRequestBuilders.get("/v1/users/1/store-reservation")
+                )
+                .andExpectAll(
+                        MockMvcResultMatchers.status().isOk(),
+                        MockMvcResultMatchers.jsonPath("code").value("000")
+                )
+                .andDo(
+                        document(
+                                "USER 팝업 스토어 예약내역 조회 성공",
+                                preprocessRequest(prettyPrint()),
+                                preprocessResponse(prettyPrint()),
+                                resource(
+                                        ResourceSnippetParameters.builder()
+                                                .tag("USER v1")
+                                                .summary("""
+                                                        ## USER 팝업 스토어 예약 내역 조회 엔드포인트 입니다.
+                                                        
+                                                        ---
+                                                        
+                                                        StoreReservation 서비스로 FeignClient 요청을 보내서 예약 내역을 받습니다.
+                                                        """)
+                                                .build()
+                                )
+
+                        )
+                );
+    }
+
+    // 한정 상품 예약 내역 조회
+    @Test
+    public void tesProductReservationGetByIdSuccess() throws Exception {
+        mockMvc.perform(
+                        RestDocumentationRequestBuilders.get("/v1/users/1/product-reservation")
+                )
+                .andExpectAll(
+                        MockMvcResultMatchers.status().isOk(),
+                        MockMvcResultMatchers.jsonPath("code").value("000")
+                )
+                .andDo(
+                        document(
+                                "USER 한정 상품 예약내역 조회 성공",
+                                preprocessRequest(prettyPrint()),
+                                preprocessResponse(prettyPrint()),
+                                resource(
+                                        ResourceSnippetParameters.builder()
+                                                .tag("USER v1")
+                                                .summary("""
+                                                        ## USER 한정 상품 예약 내역 조회 엔드포인트 입니다.
+                                                        
+                                                        ---
+                                                        
+                                                        ProductReservation 서비스로 FeignClient 요청을 보내서 예약 내역을 받습니다.
+                                                        """)
+                                                .build()
+                                )
+
+                        )
+                );
+    }
+
+}
