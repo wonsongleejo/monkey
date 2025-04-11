b3e93083cf7f9bf8bef07462fb3593ad90a31905
Taehoon <lth2943@naver.com>
2025-04-11 12:58:32 +0900

    feat: 팝업스토어시간대 생성 응답 Dto

---
+++ store-service/src/main/java/com/monkey/storeservice/application/dto/response/ResStoreTimeSlotPostDtoApiV1.java
@@ -0,0 +1,38 @@
+package com.monkey.storeservice.application.dto.response;
+
+import com.monkey.storeservice.domain.article.entity.StoreTimeSlotEntity;
+import java.util.UUID;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ResStoreTimeSlotPostDtoApiV1 {
+
+  private StoreTimeSlot storeTimeSlot;
+
+  public static ResStoreTimeSlotPostDtoApiV1 of(StoreTimeSlotEntity storeTimeSlotEntity) {
+    return ResStoreTimeSlotPostDtoApiV1.builder()
+        .storeTimeSlot(StoreTimeSlot.from(storeTimeSlotEntity))
+        .build();
+  }
+
+  @Getter
+  @Builder
+  public static class StoreTimeSlot {
+
+    private UUID timeSlotId;
+
+    public static StoreTimeSlot from(StoreTimeSlotEntity storeTimeSlotEntity) {
+      return StoreTimeSlot.builder()
+          .timeSlotId(storeTimeSlotEntity.getTimeslotId())
+          .build();
+    }
+
+  }
+
+}
