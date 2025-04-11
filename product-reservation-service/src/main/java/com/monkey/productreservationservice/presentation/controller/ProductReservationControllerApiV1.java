aeed5fa53ef6433fd8ea79035e9df5b07c2c9604
jiyun <jiyyy0720@naver.com>
2025-04-11 12:22:56 +0900

    refactor: 상품 예약 시 DTO->Entity 변환 코드 수정 - 임시 필드 채워서 변환하도록 함

--- product-reservation-service/src/main/java/com/monkey/productreservationservice/presentation/controller/ProductReservationControllerApiV1.java
+++ product-reservation-service/src/main/java/com/monkey/productreservationservice/presentation/controller/ProductReservationControllerApiV1.java
@@ -2,15 +2,16 @@ package com.monkey.productreservationservice.presentation.controller;
 
 import com.monkey.commonmodule.dto.ResDTO;
 import com.monkey.productreservationservice.application.dto.request.ReqProductReservationPostDTOApiV1;
+import com.monkey.productreservationservice.application.dto.response.ResProductReservationPostByIdCancelDTOApiV1;
 import com.monkey.productreservationservice.application.dto.response.ResProductReservationPostDTOApiV1;
 import com.monkey.productreservationservice.domain.entity.ProductReservationEntity;
+import com.monkey.productreservationservice.domain.enums.ProductReservationStatus;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/v1/product-reservations")
@@ -19,12 +20,20 @@ public class ProductReservationControllerApiV1 {
     // 예약 생성
     @PostMapping("/{productId}")
     public ResponseEntity<ResDTO<ResProductReservationPostDTOApiV1>> postBy(
+            @PathVariable UUID productId,
             @RequestBody @Valid ReqProductReservationPostDTOApiV1 reqDto
             ) {
-        ProductReservationEntity productReservationEntity = reqDto.getProductReservation().toEntity();
+        // storeId, userId -> 임시로 값 받아옴
+        UUID storeId = UUID.randomUUID();
+        long userId = 123;
+
+        ProductReservationEntity productReservationEntity = reqDto.getProductReservation().toEntity(
+                productId, userId, storeId, ProductReservationStatus.PENDING_PICKUP
+        );
 
         ResProductReservationPostDTOApiV1 resDto = ResProductReservationPostDTOApiV1.of(productReservationEntity);
 
         return new ResponseEntity<>(ResDTO.success(resDto), HttpStatus.OK);
     }
+
 }
