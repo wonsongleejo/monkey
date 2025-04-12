f913218f13105ed8296089a1f40469a4a771b8ed
jiyun <jiyyy0720@naver.com>
2025-04-13 00:58:42 +0900

    test: 상품 수정 테스트 완료

--- product-service/src/test/java/com/monkey/productservice/presentation/controller/ProductControllerApiV1Test.java
+++ product-service/src/test/java/com/monkey/productservice/presentation/controller/ProductControllerApiV1Test.java
@@ -1,8 +1,10 @@
 package com.monkey.productservice.presentation.controller;
 
 import com.epages.restdocs.apispec.ResourceSnippetParameters;
+import com.epages.restdocs.apispec.SimpleType;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.monkey.productservice.application.dto.request.ReqProductPostDTOApiV1;
+import com.monkey.productservice.application.dto.request.ReqProductPutDTOApiV1;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
@@ -18,6 +20,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.util.UUID;
 
+import static com.epages.restdocs.apispec.ResourceDocumentation.parameterWithName;
 import static com.epages.restdocs.apispec.ResourceDocumentation.resource;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
@@ -61,29 +64,85 @@ public class ProductControllerApiV1Test {
                         MockMvcResultMatchers.status().isOk(),
                         MockMvcResultMatchers.jsonPath("code").value("000")
                 )
-                .andDo(document("상품 등록 성공",
-                        preprocessRequest(prettyPrint()),
-                        preprocessResponse(prettyPrint()),
-                        resource(ResourceSnippetParameters.builder()
-                                .tag("PRODUCTS v1")
-                                .summary("상품 등록")
-                                .description("""
-                                        ## 상품 등록 엔드포인트 입니다.
-                                        """)
-                                .requestFields(
-                                        fieldWithPath("product.storeId").type(JsonFieldType.STRING).description("스토어 ID"),
-                                        fieldWithPath("product.productName").type(JsonFieldType.STRING).description("상품명"),
-                                        fieldWithPath("product.price").type(JsonFieldType.NUMBER).description("상품 가격"),
-                                        fieldWithPath("product.quantity").type(JsonFieldType.NUMBER).description("상품 수량")
+                .andDo(
+                        document("상품 등록 성공",
+                                preprocessRequest(prettyPrint()),
+                                preprocessResponse(prettyPrint()),
+                                resource(ResourceSnippetParameters.builder()
+                                        .tag("PRODUCTS v1")
+                                        .summary("상품 등록")
+                                        .description("""
+                                                ## 상품 등록 엔드포인트 입니다.
+                                                """)
+                                        .requestFields(
+                                                fieldWithPath("product.storeId").type(JsonFieldType.STRING).description("스토어 ID"),
+                                                fieldWithPath("product.productName").type(JsonFieldType.STRING).description("상품명"),
+                                                fieldWithPath("product.price").type(JsonFieldType.NUMBER).description("상품 가격"),
+                                                fieldWithPath("product.quantity").type(JsonFieldType.NUMBER).description("상품 수량")
+                                        )
+                                        .responseFields(
+                                                fieldWithPath("code").type(JsonFieldType.STRING).description("응답 코드"),
+                                                fieldWithPath("message").type(JsonFieldType.STRING).description("응답 메시지"),
+                                                fieldWithPath("data.product.productId").type(JsonFieldType.STRING).optional().description("상품 ID")
+                                        )
+                                        .build()
                                 )
-                                .responseFields(
-                                        fieldWithPath("code").type(JsonFieldType.STRING).description("응답 코드"),
-                                        fieldWithPath("message").type(JsonFieldType.STRING).description("응답 메시지"),
-                                        fieldWithPath("data.product.productId").type(JsonFieldType.STRING).optional().description("상품 ID")
-                                )
-                                .build()
                         )
-                ));
+                );
+    }
+
+    // 상품 수정
+    @Test
+    public void testProductPutSuccess() throws Exception {
+        ReqProductPutDTOApiV1 reqDto = ReqProductPutDTOApiV1.builder()
+                .product(
+                        ReqProductPutDTOApiV1.Product.builder()
+                                .productName("테스트 상품 ver2")
+                                .price(50000)
+                                .quantity(200)
+                                .build()
+                )
+                .build();
+        String reqDtoJson = objectMapper.writeValueAsString(reqDto);
+        mockMvc.perform(
+                RestDocumentationRequestBuilders.put("/v1/products/{productId}", UUID.randomUUID())
+//                                .header(HttpHeaders.AUTHORIZATION, "Bearer " + resDto.getData().getAccessJwt())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(reqDtoJson)
+                )
+                .andExpectAll(
+                        MockMvcResultMatchers.status().isOk(),
+                        MockMvcResultMatchers.jsonPath("code").value("000")
+                )
+                .andDo(
+                        document("상품 수정 성공",
+                                preprocessRequest(prettyPrint()),
+                                preprocessResponse(prettyPrint()),
+                                resource(ResourceSnippetParameters.builder()
+                                        .tag("PRODUCTS v1")
+                                        .summary("상품 수정")
+                                        .description("""
+                                                ## 상품 수정 엔드포인트 입니다.
+                                                """)
+                                        .pathParameters(
+                                                parameterWithName("productId").type(SimpleType.STRING).description("상품 ID")
+                                        )
+                                        .requestFields(
+                                                fieldWithPath("product.productName").type(JsonFieldType.STRING).description("상품명"),
+                                                fieldWithPath("product.price").type(JsonFieldType.NUMBER).description("상품 가격"),
+                                                fieldWithPath("product.quantity").type(JsonFieldType.NUMBER).description("상품 수량")
+                                        )
+                                        .responseFields(
+                                                fieldWithPath("code").type(JsonFieldType.STRING).description("응답 코드"),
+                                                fieldWithPath("message").type(JsonFieldType.STRING).description("응답 메시지"),
+                                                fieldWithPath("data.product.productName").type(JsonFieldType.STRING).description("상품명"),
+                                                fieldWithPath("data.product.price").type(JsonFieldType.NUMBER).description("상품 가격"),
+                                                fieldWithPath("data.product.quantity").type(JsonFieldType.NUMBER).description("상품 수량")
+                                        )
+                                        .build()
+                                )
+                        )
+                );
     }
 
 }
