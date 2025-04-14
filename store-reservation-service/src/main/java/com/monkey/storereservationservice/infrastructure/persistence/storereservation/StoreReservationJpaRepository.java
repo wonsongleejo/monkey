432deec1e33fab549db671bb83b3748102c2fd6a
yejiscore <yejisong24@gmail.com>
2025-04-14 23:14:22 +0900

    feat: 팝업스토어 예약 Repository 구현

---
+++ store-reservation-service/src/main/java/com/monkey/storereservationservice/infrastructure/persistence/storereservation/StoreReservationJpaRepository.java
@@ -0,0 +1,9 @@
+package com.monkey.storereservationservice.infrastructure.persistence.storereservation;
+
+import com.monkey.storereservationservice.domain.StoreReservation.entity.StoreReservationEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface StoreReservationJpaRepository extends JpaRepository<StoreReservationEntity, UUID> {
+}
