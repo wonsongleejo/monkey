05a21af0a2f49f2cc28de4777bbdc7dda2ccca4f
jiyun <jiyyy0720@naver.com>
2025-04-14 11:48:50 +0900

    test: 상품 예약 상세조회 테스트 작성 완료

--- product-reservation-service/src/test/java/com/monkey/productreservationservice/presentation/controller/ProductReservationControllerApiV1Test.java
+++ product-reservation-service/src/test/java/com/monkey/productreservationservice/presentation/controller/ProductReservationControllerApiV1Test.java
@@ -126,5 +126,49 @@ public class ProductReservationControllerApiV1Test {
     }
 
     // 예약 상세 조회
+    @Test
+    public void testProductReservationGetByIdSuccess() throws Exception {
+        mockMvc.perform(
+                RestDocumentationRequestBuilders.get("/v1/product-reservations/{productReservationId}", UUID.randomUUID())
+                )
+                .andExpectAll(
+                        MockMvcResultMatchers.status().isOk(),
+                        MockMvcResultMatchers.jsonPath("code").value("000")
+                )
+                .andDo(
+                        document("상품 예약 상세 조회 성공",
+                                preprocessRequest(prettyPrint()),
+                                preprocessResponse(prettyPrint()),
+                                resource(ResourceSnippetParameters.builder()
+                                        .tag("PRODUCT Reservations v1")
+                                        .summary("상품 예약 상세 조회")
+                                        .description("""
+                                                ## 상품 예약 상세 조회 엔드포인트 입니다.
+                                                """)
+                                        .pathParameters(
+                                                parameterWithName("productReservationId").type(SimpleType.STRING).description("상품 예약 ID")
+                                        )
+                                        .responseFields(
+                                                fieldWithPath("code").type(JsonFieldType.STRING).description("응답 코드"),
+                                                fieldWithPath("message").type(JsonFieldType.STRING).description("응답 메시지"),
+                                                fieldWithPath("data.productReservation.productReservationId").type(JsonFieldType.STRING).description("상품 예약 ID"),
+                                                fieldWithPath("data.productReservation.quantity").type(JsonFieldType.NUMBER).description("예약 수량"),
+                                                fieldWithPath("data.productReservation.status").type(JsonFieldType.STRING).description("예약 상태"),
+                                                fieldWithPath("data.productReservation.createdAt").type(JsonFieldType.STRING).description("예약 생성일시"),
+
+                                                fieldWithPath("data.productReservation.product.productId").type(JsonFieldType.STRING).description("상품 ID"),
+                                                fieldWithPath("data.productReservation.product.productName").type(JsonFieldType.STRING).description("상품명"),
+
+                                                fieldWithPath("data.productReservation.user.userId").type(JsonFieldType.NUMBER).description("유저 ID"),
+                                                fieldWithPath("data.productReservation.user.userName").type(JsonFieldType.STRING).description("유저 이름"),
+
+                                                fieldWithPath("data.productReservation.store.storeId").type(JsonFieldType.STRING).description("스토어 ID"),
+                                                fieldWithPath("data.productReservation.store.storeName").type(JsonFieldType.STRING).description("스토어 이름")
+                                        )
+                                        .build()
+                                )
+                        )
+                );
+    }
 }
 
