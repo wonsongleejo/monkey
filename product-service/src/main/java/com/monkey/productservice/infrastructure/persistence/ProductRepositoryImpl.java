682f388e6989e0eaed903b0638b9b412ba76837c
jiyun <jiyyy0720@naver.com>
2025-04-15 11:01:40 +0900

    refactor: 상품 Repository 계층 분리 및 컨트롤러 수정

---
+++ product-service/src/main/java/com/monkey/productservice/infrastructure/persistence/ProductRepositoryImpl.java
@@ -0,0 +1,31 @@
+package com.monkey.productservice.infrastructure.persistence;
+
+import com.monkey.productservice.domain.entity.ProductEntity;
+import com.monkey.productservice.domain.repository.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+@RequiredArgsConstructor
+public class ProductRepositoryImpl implements ProductRepository {
+    private final ProductJpaRepository productJpaRepository;
+
+    @Override
+    public ProductEntity save(ProductEntity productEntity) {
+        return productJpaRepository.save(productEntity);
+    }
+
+    @Override
+    public Optional<ProductEntity> findByIdAndIsDeletedFalse(UUID id) {
+        return productJpaRepository.findByIdAndIsDeletedFalse(id);
+    }
+
+    @Override
+    public List<ProductEntity> findAllByIsDeletedFalse() {
+        return productJpaRepository.findAllByIsDeletedFalse();
+    }
+}
