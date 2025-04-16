be2b5a709354efb12ae565f6e0bc75f65f81e000
jiyun <jiyyy0720@naver.com>
2025-04-17 03:15:28 +0900

    feat: 상품 예약 FeignClient 인터페이스/ResDTO 생성

---
+++ product-reservation-service/src/main/java/com/monkey/productreservationservice/infrastructure/feignclient/dto/ResStoreClientGetByIdDTOApiV1.java
@@ -0,0 +1,17 @@
+package com.monkey.productreservationservice.infrastructure.feignclient.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ResStoreClientGetByIdDTOApiV1 {
+    private UUID storeId;
+    private String storeName;
+}
