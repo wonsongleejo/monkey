a71de762fa17470778868ea0a4ca6a679366d523
jiyun <jiyyy0720@naver.com>
2025-04-17 14:27:36 +0900

    feat: 상품예약 - 상품, 스토어, 유저 FeignClient

--- product-reservation-service/src/main/java/com/monkey/productreservationservice/infrastructure/feignclient/ProductFeignClientApiV1.java
+++ product-reservation-service/src/main/java/com/monkey/productreservationservice/infrastructure/feignclient/ProductFeignClientApiV1.java
@@ -1,15 +1,15 @@
 package com.monkey.productreservationservice.infrastructure.feignclient;
 
 import com.monkey.common_module.dto.ResDTO;
-import com.monkey.productreservationservice.infrastructure.feignclient.dto.response.ResStoreClientGetByIdDTOApiV1;
+import com.monkey.productreservationservice.infrastructure.feignclient.dto.response.ResProductClientGetByIdDTOApiV1;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.UUID;
 
-@FeignClient(name = "store-service", path = "v1/stores")
-public interface StoreFeignClientApiV1 {
+@FeignClient(name = "product-service", path = "v1/products")
+public interface ProductFeignClientApiV1 {
     @GetMapping
-    ResDTO<ResStoreClientGetByIdDTOApiV1> getStoreById(@PathVariable UUID storeId);
+    ResDTO<ResProductClientGetByIdDTOApiV1> getProductById(@PathVariable UUID ProductId);
 }
