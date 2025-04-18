3a3c2427bcdaf784b9c864dffbc3f4b7a03d128b
jiyun <jiyyy0720@naver.com>
2025-04-18 20:30:17 +0900

    feat: 스토어 예약 FeignClient 인터페이스/DTO 추가

---
+++ product-reservation-service/src/main/java/com/monkey/productreservationservice/infrastructure/feignclient/StoreReservationFeignClientApiV1.java
@@ -0,0 +1,13 @@
+package com.monkey.productreservationservice.infrastructure.feignclient;
+
+import com.monkey.common_module.dto.ResDTO;
+import com.monkey.productreservationservice.infrastructure.feignclient.dto.response.ResStoreReservationClientGetDTOApiV1;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@FeignClient(name = "store-reservation-service", path = "v1/store-reservations")
+public interface StoreReservationFeignClientApiV1 {
+    @GetMapping
+    ResDTO<ResStoreReservationClientGetDTOApiV1> getReservationsByUserId(@RequestParam Long userId);
+}
---
+++ product-reservation-service/src/main/java/com/monkey/productreservationservice/infrastructure/feignclient/dto/response/ResStoreReservationClientGetDTOApiV1.java
@@ -0,0 +1,24 @@
+package com.monkey.productreservationservice.infrastructure.feignclient.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ResStoreReservationClientGetDTOApiV1 {
+    private List<StoreReservation> storeReservationList;
+
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @Builder
+    public static class StoreReservation {
+        private Long userId;
+    }
+}
