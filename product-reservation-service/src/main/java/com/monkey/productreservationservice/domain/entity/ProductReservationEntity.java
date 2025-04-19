f4eb2b242935ac737a0c932dbd0832ec66ce4117
jiyun <jiyyy0720@naver.com>
2025-04-20 00:55:58 +0900

    feat: 상품 예약 엔티티에 cancel() 메서드 추가

--- product-reservation-service/src/main/java/com/monkey/productreservationservice/domain/entity/ProductReservationEntity.java
+++ product-reservation-service/src/main/java/com/monkey/productreservationservice/domain/entity/ProductReservationEntity.java
@@ -1,6 +1,8 @@
 package com.monkey.productreservationservice.domain.entity;
 
+import com.monkey.common_module.dto.ResponseCode;
 import com.monkey.common_module.entity.BaseEntity;
+import com.monkey.common_module.exception.CustomException;
 import com.monkey.productreservationservice.domain.vo.ProductReservationStatus;
 import jakarta.persistence.*;
 import lombok.*;
@@ -35,4 +37,12 @@ public class ProductReservationEntity extends BaseEntity {
     @Enumerated(EnumType.STRING)
     @Column(nullable = false)
     private ProductReservationStatus status;
+
+    public void cancel(long userId) {
+        if(this.getIsDeleted() || this.status == ProductReservationStatus.CANCELED || this.status == ProductReservationStatus.PICKED_UP) {
+            throw new CustomException(ResponseCode.DUPLICATED_REQUEST);
+        }
+        this.status = ProductReservationStatus.CANCELED;
+        this.delete(userId);
+    }
 }
