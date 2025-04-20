2feffd3417471e2cca18561f3f945b371975a0f2
Taehoon <lth2943@naver.com>
2025-04-20 22:02:05 +0900

    feat : 팝업스토어 시간대 서비스 구현

---
+++ store-service/src/main/java/com/monkey/storeservice/application/service/StoreTimeSlotServiceImplApiV1.java
@@ -0,0 +1,65 @@
+package com.monkey.storeservice.application.service;
+
+import com.monkey.common_module.dto.ResponseCode;
+import com.monkey.common_module.exception.CustomException;
+import com.monkey.storeservice.application.dto.request.ReqStoreTimeSlotPostDTOApiV1;
+import com.monkey.storeservice.application.dto.request.ReqStoreTimeSlotPutDTOApiV1;
+import com.monkey.storeservice.application.dto.response.ResStoreTimeSlotGetDTOApiV1;
+import com.monkey.storeservice.application.dto.response.ResStoreTimeSlotPostDTOApiV1;
+import com.monkey.storeservice.application.dto.response.ResStoreTimeSlotPutDTOApiV1;
+import com.monkey.storeservice.domain.entity.StoreTimeSlotEntity;
+import com.monkey.storeservice.domain.repository.StoreTimeSlotRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Page;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class StoreTimeSlotServiceImplApiV1 implements StoreTimeSlotServiceApiV1 {
+
+  private final StoreTimeSlotRepository storeTimeSlotRepository;
+
+  // 생성
+  @Override
+  public ResStoreTimeSlotPostDTOApiV1 postById(UUID storeId, ReqStoreTimeSlotPostDTOApiV1 reqDto) {
+
+    StoreTimeSlotEntity storeTimeSlotEntity = reqDto.getStoreTimeSlot().toStoreTimeSlotEntity();
+
+    storeTimeSlotEntity.setStoreId(storeId);
+
+    StoreTimeSlotEntity saved = storeTimeSlotRepository.save(storeTimeSlotEntity);
+
+    return ResStoreTimeSlotPostDTOApiV1.of(saved);
+  }
+
+  // 수정
+  @Override
+  public ResStoreTimeSlotPutDTOApiV1 putById(UUID timeSlotId, ReqStoreTimeSlotPutDTOApiV1 reqDto) {
+
+    StoreTimeSlotEntity storeTimeSlotEntity = storeTimeSlotRepository.findById(timeSlotId)
+        .orElseThrow(() -> new CustomException(ResponseCode.NOT_FOUND));
+
+    StoreTimeSlotEntity updated = storeTimeSlotRepository.save(storeTimeSlotEntity);
+    return ResStoreTimeSlotPutDTOApiV1.of(updated);
+  }
+
+  // 단건 조회
+  @Override
+  public ResStoreTimeSlotGetDTOApiV1 getById(UUID timeSlotId) {
+
+    StoreTimeSlotEntity storeTimeSlotEntity = storeTimeSlotRepository.findById(timeSlotId)
+        .orElseThrow(() -> new CustomException(ResponseCode.NOT_FOUND));
+
+    return ResStoreTimeSlotGetDTOApiV1.of(storeTimeSlotEntity);
+  }
+
+  // 전체 조회
+  @Override
+  public ResStoreTimeSlotGetDTOApiV1 getBy(Pageable pageable) {
+    Page<StoreTimeSlotEntity> page = storeTimeSlotRepository.findAll(pageable);
+    return ResStoreTimeSlotGetDTOApiV1.of(page);
+  }
+}
