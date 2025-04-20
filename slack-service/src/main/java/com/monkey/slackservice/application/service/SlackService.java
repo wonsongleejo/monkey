aab53c5336c6ca81e63fdad8563fc69b7cd5ea79
yejiscore <yejisong24@gmail.com>
2025-04-20 22:39:46 +0900

    feat: Slack 도메인 Service 구현

---
+++ slack-service/src/main/java/com/monkey/slackservice/application/service/SlackService.java
@@ -0,0 +1,11 @@
+package com.monkey.slackservice.application.service;
+
+import com.monkey.slackservice.application.dto.request.ReqSlackStoreReservationPostDTOApiV1;
+import com.monkey.slackservice.application.dto.response.ResSlackStoreReservationPostDTOApiV1;
+
+public interface SlackService {
+    ResSlackStoreReservationPostDTOApiV1 notifySlackAndSave(
+            ReqSlackStoreReservationPostDTOApiV1 request,
+            ResSlackStoreReservationPostDTOApiV1.StoreReservation reservationInfo
+    );
+}
