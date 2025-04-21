b2dfbd414c6c9f1ae3fe955f3510f2632200c7ce
jiyun <jiyyy0720@naver.com>
2025-04-21 11:40:05 +0900

    fix: FeignClient 인터페이스에 @Profile("!test") 추가

--- product-service/src/main/java/com/monkey/productservice/infrastructure/feignclient/StoreFeignClientApiV1.java
+++ product-service/src/main/java/com/monkey/productservice/infrastructure/feignclient/StoreFeignClientApiV1.java
@@ -3,12 +3,14 @@ package com.monkey.productservice.infrastructure.feignclient;
 import com.monkey.common_module.dto.ResDTO;
 import com.monkey.productservice.infrastructure.feignclient.dto.response.ResStoreClientGetByIdDTOApiV1;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.context.annotation.Profile;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.UUID;
 
 @FeignClient(name = "store-service", path = "v1/stores")
+@Profile("!test")
 public interface StoreFeignClientApiV1 {
     @GetMapping
     ResDTO<ResStoreClientGetByIdDTOApiV1> getStoreById(@PathVariable("storeId") UUID storeId);
