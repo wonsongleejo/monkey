9cce77d3edf98b2e347c33bf7c5dfa4475c9b1be
cho <websera12@gmail.com>
2025-04-21 20:07:24 +0900

    refactor: 회원 정보 수정, Valid 적용, 예외 실패 시 응답값

--- user-service/src/main/java/com/monkey/userservice/application/service/UserServiceImplApiV1.java
+++ user-service/src/main/java/com/monkey/userservice/application/service/UserServiceImplApiV1.java
@@ -3,12 +3,14 @@ package com.monkey.userservice.application.service;
 import com.monkey.userservice.application.dto.request.ReqUserPutDTOApiV1;
 import com.monkey.userservice.domain.entity.UserEntity;
 import com.monkey.userservice.domain.repository.UserRepository;
+import com.monkey.userservice.infrastructure.client.StoreReservationFeignClientApiV1;
 import com.querydsl.core.types.Predicate;
 import lombok.RequiredArgsConstructor;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @Transactional(readOnly = true)
@@ -16,6 +18,8 @@ import org.springframework.stereotype.Service;
 public class UserServiceImplApiV1 implements UserServiceApiV1 {
 
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final StoreReservationFeignClientApiV1 storeReservationFeignClientApiV1;
 
     @Override
     public Page<UserEntity> getBy(Predicate predicate, Pageable pageable) {
@@ -35,7 +39,9 @@ public class UserServiceImplApiV1 implements UserServiceApiV1 {
         UserEntity user = userRepository.findByIsDeletedFalse(userId)
                 .orElseThrow(()-> new IllegalArgumentException("회원이 존재하지 않습니다."));
 
-        reqDto.getUser().update(user);
+        // 비밀번호 암호화
+        String encryptedPassword = passwordEncoder.encode(reqDto.getUser().getPassword());
+        user.update(encryptedPassword, reqDto.getUser().getSlackId());
 
         return user;
     }
