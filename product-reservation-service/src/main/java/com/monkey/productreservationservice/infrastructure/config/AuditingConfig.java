07155c7f1652dd7338745e5eb5782d0d0b4e69e7
jiyun <jiyyy0720@naver.com>
2025-04-22 18:57:37 +0900

    feat: AuditingConfig 클래스 추가

---
+++ product-reservation-service/src/main/java/com/monkey/productreservationservice/infrastructure/config/AuditingConfig.java
@@ -0,0 +1,16 @@
+package com.monkey.productreservationservice.infrastructure.config;
+
+import com.monkey.common_module.entity.AuditorAwareImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing(auditorAwareRef = "auditorProvider")
+public class AuditingConfig {
+    @Bean(name = "auditorProvider")
+    public AuditorAware<Long> auditorProvider() {
+        return new AuditorAwareImpl();
+    }
+}
