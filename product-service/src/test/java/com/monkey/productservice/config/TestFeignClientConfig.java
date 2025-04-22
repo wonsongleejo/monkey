0e2dc1c42a47e9fd872332cce8644767576155aa
jiyun <jiyyy0720@naver.com>
2025-04-22 11:11:21 +0900

    fix: FeignClient 의존성 문제(테스트 실행 에러) 해결

--- product-service/src/test/java/com/monkey/productservice/config/TestFeignClientConfig.java
+++ product-service/src/test/java/com/monkey/productservice/config/TestFeignClientConfig.java
@@ -3,9 +3,11 @@ package com.monkey.productservice.config;
 import com.monkey.productservice.infrastructure.feignclient.StoreFeignClientApiV1;
 import com.monkey.productservice.infrastructure.feignclient.UserFeignClientApiV1;
 import org.mockito.Mockito;
+import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Primary;
 
+@TestConfiguration
 public class TestFeignClientConfig {
     @Bean
     @Primary
