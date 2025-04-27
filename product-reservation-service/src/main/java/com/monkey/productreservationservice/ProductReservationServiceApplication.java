aa4d0d702eb85c24d5daaaf515cf328804d1de31
cho <websera12@gmail.com>
2025-04-27 22:14:50 +0900

    refactor: 한정 상품 개인 예약내역 조회 기능에 Redis 적용
    - Redis 설정(config, dependencies 추가)
    - v2로 디렉토리를 추가하여 service 와 controller 파일 수정

--- product-reservation-service/src/main/java/com/monkey/productreservationservice/ProductReservationServiceApplication.java
+++ product-reservation-service/src/main/java/com/monkey/productreservationservice/ProductReservationServiceApplication.java
@@ -2,10 +2,12 @@ package com.monkey.productreservationservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication(scanBasePackages = "com.monkey")
 @EnableFeignClients
+@EnableCaching
 public class ProductReservationServiceApplication {
 
     public static void main(String[] args) {
